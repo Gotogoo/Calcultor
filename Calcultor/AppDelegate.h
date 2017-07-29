33ce7c97a11974b1c63f3964e596ccaee3530943
Gotogoo <fcliang@thoughtworks.com>
2017-07-29 17:06:38 +0800

    Initial Commit

---
+++ Calcultor/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Calcultor
+//
+//  Created by Facheng Liang  on 2017/7/29.
+//  Copyright © 2017年 Facheng Liang . All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Calcultor/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Calcultor
+//
+//  Created by Facheng Liang  on 2017/7/29.
+//  Copyright © 2017年 Facheng Liang . All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
